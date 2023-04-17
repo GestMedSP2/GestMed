05724db0f30c7c003e78fd04cc1d0ad46ffd59c5
AikoJu <julya.aono@sptech.school>
2023-04-16 22:25:39 -0300

    Atualização no dashboard dos gráficos

--- siteInstitucional/js/charts.js
+++ siteInstitucional/js/charts.js
@@ -1,3 +1,6 @@
+// geral
+Chart.defaults.font.family = 'Poppins';
+
 const horario = [
         '12:00',
         '13:00',
@@ -7,10 +10,7 @@ const horario = [
         '17:00',
     ]
 
-    const temperatura = [30, 29, 28, 25, 22, 23];
-    const umidade = [80, 82, 80, 85, 80, 83];
-
-    const lineChart = document.getElementById('myLineChart');
+//Linha pontilhada
 
     const horizontalDottedLine = {
         id: 'horizontalDottedLine', 
@@ -27,8 +27,11 @@ const horario = [
         }
     }
 
-    Chart.defaults.font.family = 'Poppins';
+   
+// Gráfico temperatura
 
+    const lineChart = document.getElementById('myLineChart');
+    const temperatura = [5, 5, 3, 4, 6, 10];
     new Chart(lineChart, {
         data: {
             datasets: [
@@ -38,7 +41,22 @@ const horario = [
                     data: temperatura,
                     backgroundColor: '#50C37E',
                     borderColor: '#50C37E'
-                },
+                }
+            ],
+            labels: horario
+        },
+        options: {
+        },
+        plugins: [horizontalDottedLine]
+    });
+
+// Gráfico umidade
+
+    const umidade = [42, 50, 50, 55, 62, 66];
+    const lineChart2 = document.getElementById('myLineChart2');
+    new Chart(lineChart2, {
+        data: {
+            datasets: [
                 {
                     type: 'line',
                     label: 'Umidade',
@@ -47,21 +65,16 @@ const horario = [
                     borderColor: '#708BFF'
                 }
             ],
-            labels: horario,
+            labels: horario
         },
         options: {
         },
         plugins: [horizontalDottedLine]
     });
 
-    const mes = [
-        '1:00',
-        '2:00',
-        '3:00',
-        '4:00',
-        '5:00',
-        '6:00',
-    ]
+
+
+// Gráfico média
 
     const temperaturaMedia = [22, 24, 27, 23, 20,18];
     const umidadeMedia = [90, 89, 93, 87, 88 , 70];
@@ -86,34 +99,6 @@ const horario = [
                     borderRadius: 10,
                 }
             ],
-            labels: mes
+            labels: horario
         },
-    });
-
-
-    
-    const lineChart2 = document.getElementById('myLineChart2');
-    new Chart(lineChart2, {
-        data: {
-            datasets: [
-                {
-                    type: 'line',
-                    label: 'Temperatura',
-                    data: temperatura,
-                    backgroundColor: '#50C37E',
-                    borderColor: '#50C37E'
-                },
-                {
-                    type: 'line',
-                    label: 'Umidade',
-                    data: umidade,
-                    backgroundColor: '#708BFF',
-                    borderColor: '#708BFF'
-                }
-            ],
-            labels: horario,
-        },
-        options: {
-        },
-        plugins: [horizontalDottedLine]
     });
