ac77bd5dfdd949cf2710bd1807c51567235941a0
Diego <diegosousa241104@gmail.com>
2023-06-04 23:58:29 -0300

    Adicionando responsividade as telas de alertas e setores

--- src/database/config.js
+++ src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "gestmed",
-    user: "sptech",
-    password: "030204",
+    user: "root",
+    password: "admin",
 };
 
 function executar(instrucao) {
